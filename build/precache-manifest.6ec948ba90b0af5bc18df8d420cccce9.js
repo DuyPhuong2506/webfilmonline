a79b077c05ed1a91993b8580fc49b093005d8b39
Duy Phuong <daop37566@gmail.com>
2021-11-18 21:01:38 +0700

    initial Code 3

---
+++ build/precache-manifest.6ec948ba90b0af5bc18df8d420cccce9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707a7b5d0a8ac88ebfe4b9ac7c7f262e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30afdf801df15ab5e5d3",
+    "url": "/static/css/2.87eacd4f.chunk.css"
+  },
+  {
+    "revision": "404d13df75609d54cb9e",
+    "url": "/static/css/main.46b442ec.chunk.css"
+  },
+  {
+    "revision": "30afdf801df15ab5e5d3",
+    "url": "/static/js/2.5357e105.chunk.js"
+  },
+  {
+    "revision": "b78d06d995050d23ec508cb4ba84482c",
+    "url": "/static/js/2.5357e105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404d13df75609d54cb9e",
+    "url": "/static/js/main.8903860c.chunk.js"
+  },
+  {
+    "revision": "464715772da00271490d",
+    "url": "/static/js/runtime-main.c45bf4e4.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2e24b951d4e8168b4f8de997d870861e",
+    "url": "/static/media/footer-bg.2e24b951.jpg"
+  },
+  {
+    "revision": "dd9de802f4a818ba7cdf5400eb4b4a16",
+    "url": "/static/media/tmovie.dd9de802.png"
+  }
+]);
